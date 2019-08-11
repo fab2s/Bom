92c4a6a2a420c7e769f6f01a36b30b726f35c4c3
fab2s <fabrice.de.stefanis@gmail.com>
2019-08-11 13:24:18 +0200

    Initial upload

---
+++ README.md
@@ -0,0 +1,52 @@
+# Bom
+
+[![Build Status](https://travis-ci.org/fab2s/Bom.svg?branch=master)](https://travis-ci.org/fab2s/Bom) [![Latest Stable Version](https://poser.pugx.org/fab2s/bom/v/stable)](https://packagist.org/packages/fab2s/bom) [![Scrutinizer Code Quality](https://scrutinizer-ci.com/g/fab2s/Bom/badges/quality-score.png?b=master)](https://scrutinizer-ci.com/g/fab2s/Bom/?branch=master) [![PRs Welcome](https://img.shields.io/badge/PRs-welcome-brightgreen.svg?style=flat)](http://makeapullrequest.com) [![License](https://poser.pugx.org/fab2s/bom/license)](https://packagist.org/packages/fab2s/bom)
+
+A purely static zero dependencies [byte Order Mark](https://en.wikipedia.org/wiki/Byte_order_mark) Helper to handle unicode BOMs
+
+## Installation
+
+`Bom` can be installed using composer:
+
+```
+composer require "fab2s/bom"
+```
+
+`Bom` is also included in [OpinHelper](https://github.com/fab2s/OpinHelpers) which packages several bellow "Swiss Army Knife" level Helpers covering some of the most annoying aspects of php programing, such as UTF8 string manipulation, high precision Mathematics or properly locking a file
+
+## Prerequisites
+
+`Bom` has no specific dependencies 
+
+## In practice
+
+Bom is minimalistic, it only exposes four methods. It supports `UTF-8|16|32` BOMs in both [_Little_](https://en.wikipedia.org/wiki/Endianness#Little) and [_Big_](https://en.wikipedia.org/wiki/Endianness#Big) [Endianness](https://en.wikipedia.org/wiki/Endianness)
+
+```php
+// get Bom
+$bomAtStartOfString = Bom::extract($string); // returns null when none are found
+
+// remove eventual BOM from beging of string
+$bomLessString = Bom::drop($string);
+
+// get encoding from a supported BOM
+$encoding = Bom::getBomEncoding($bom); // returns null if the BOM is not valid
+
+// get BOM for a supported encoding
+$bom = Bom::getEncodingBom($encoding); // returns null if the encoding is not supported (has no BOM)
+
+// get the list of BOMs as an array with corresponding encodings as indexes
+$boms = Bom::getBoms(); // returns an associatiove array of encodings and correesponding BOMs
+```
+
+## Requirements
+
+Math is tested against php 7.1, 7.2 and 7.3.
+
+## Contributing
+
+Contributions are welcome, do not hesitate to open issues and submit pull requests.
+
+## License
+
+SoUuid is open-sourced software licensed under the [MIT license](https://opensource.org/licenses/MIT).
---
+++ src/Bom.php
@@ -0,0 +1,121 @@
+<?php
+
+/*
+ * This file is part of Bom.
+ *     (c) Fabrice de Stefanis / https://github.com/fab2s/Bom
+ * This source file is licensed under the MIT license which you will
+ * find in the LICENSE file or at https://opensource.org/licenses/MIT
+ */
+
+namespace fab2s\Bom;
+
+/**
+ * class Bom
+ */
+class Bom
+{
+    /**
+     * UTF8 BOM & encoding
+     */
+    const BOM_UTF8 = "\xEF\xBB\xBF";
+    const ENC_UTF8 = 'UTF-8';
+
+    /**
+     * UTF16_BE BOM & encoding
+     */
+    const BOM_UTF16_BE = "\xFE\xFF";
+    const ENC_UTF16_BE = 'UTF-16BE';
+
+    /**
+     * UTF16_LE BOM & encoding
+     */
+    const BOM_UTF16_LE = "\xFF\xFE";
+    const ENC_UTF16_LE = 'UTF-16LE';
+
+    /**
+     * UTF32_BE BOM & encoding
+     */
+    const BOM_UTF32_BE = "\x00\x00\xFE\xFF";
+    const ENC_UTF32_BE = 'UTF-32BE';
+
+    /**
+     * UTF32_LE BOM & encoding
+     */
+    const BOM_UTF32_LE = "\xFF\xFE\x00\x00";
+    const ENC_UTF32_LE = 'UTF-32LE';
+
+    /**
+     * UTF8 | UTF16_BE | UTF32_LE | UTF16_LE | UTF32_BE
+     */
+    const BOM_REGEX = '\xEF\xBB\xBF|\xFE\xFF|\xFF\xFE\x00\x00|\xFF\xFE|\x00\x00\xFE\xFF';
+
+    /**
+     * @var string[]
+     */
+    protected static $boms = [
+        self::ENC_UTF8     => self::BOM_UTF8,
+        self::ENC_UTF16_BE => self::BOM_UTF16_BE,
+        self::ENC_UTF16_LE => self::BOM_UTF16_LE,
+        self::ENC_UTF32_BE => self::BOM_UTF32_BE,
+        self::ENC_UTF32_LE => self::BOM_UTF32_LE,
+    ];
+
+    /**
+     * @var string[]
+     */
+    protected static $smob = [
+        self::BOM_UTF8     => self::ENC_UTF8,
+        self::BOM_UTF16_BE => self::ENC_UTF16_BE,
+        self::BOM_UTF16_LE => self::ENC_UTF16_LE,
+        self::BOM_UTF32_BE => self::ENC_UTF32_BE,
+        self::BOM_UTF32_LE => self::ENC_UTF32_LE,
+    ];
+
+    /**
+     * @param string $string
+     *
+     * @return string|null
+     */
+    public static function extract(string $string): ? string
+    {
+        return preg_match('`^(' . static::BOM_REGEX . ')`', $string, $match) ? $match[1] : null;
+    }
+
+    /**
+     * @param string $string
+     *
+     * @return string
+     */
+    public static function drop(string $string): string
+    {
+        return preg_replace('`^(' . static::BOM_REGEX . ')`', '', $string);
+    }
+
+    /**
+     * @param string $bom
+     *
+     * @return string|null
+     */
+    public static function getBomEncoding(string $bom): ? string
+    {
+        return static::$smob[$bom] ?? null;
+    }
+
+    /**
+     * @param string $encoding
+     *
+     * @return null|string
+     */
+    public static function getEncodingBom(string $encoding): ? string
+    {
+        return static::$boms[$encoding] ?? null;
+    }
+
+    /**
+     * @return array
+     */
+    public static function getBoms(): array
+    {
+        return static::$boms;
+    }
+}
